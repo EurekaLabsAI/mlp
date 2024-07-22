64ae102c647026ae4f38856aff7d51d0c813320a
karpathy <andrej.karpathy@gmail.com>
2024-07-21 17:49:23 -0700

    make training consistent with pytorch more, and nicer, and fix some bugs around e.g. lr handing inside AdamW

--- mlp_numpy.py
+++ mlp_numpy.py
@@ -1,13 +1,15 @@
 """
 Implements a simple n-gram language model in NumPy.
+PyTorch has the Autograd engine, which calculates the gradients for us.
+But in NumPy, we have to do the backward pass manually.
 """
 import math
 import numpy as np
 
-from common import RNG
+from common import RNG, StepTimer
 
 # -----------------------------------------------------------------------------
-# The NumPy Module
+# NumPy implementation of a Multilayer Perceptron (MLP)
 
 class MLP:
     """
@@ -18,23 +20,19 @@ class MLP:
     Bengio et al. 2003 https://www.jmlr.org/papers/volume3/bengio03a/bengio03a.pdf
     """
 
-    def __init__(self, random: RNG, vocab_size, context_length, embedding_size, hidden_size):
+    def __init__(self, rng, vocab_size, context_length, embedding_size, hidden_size):
         v, t, e, h = vocab_size, context_length, embedding_size, hidden_size
-        self.E = e
-
+        self.embedding_size = embedding_size
         # initialize the weights the same way PyTorch does
-        self.wte = np.asarray(random.randn(v * e, mu=0, sigma=1.0)).reshape(v, e)
+        self.wte = np.asarray(rng.randn(v * e, mu=0, sigma=1.0), dtype=np.float32).reshape(v, e)
         scale = 1 / math.sqrt(e * t)
-        self.fc1_weights = np.asarray(random.rand(t * e * h, -scale, scale)).reshape(h, t * e).T
-        self.fc1_bias = np.asarray(random.rand(h, -scale, scale))
+        self.fc1_weights = np.asarray(rng.rand(t * e * h, -scale, scale), dtype=np.float32).reshape(h, t * e).T
+        self.fc1_bias = np.asarray(rng.rand(h, -scale, scale), dtype=np.float32)
         scale = 1 / math.sqrt(h)
-        self.fc2_weights = np.asarray(random.rand(h * v, -scale, scale)).reshape(v, h).T
-        self.fc2_bias = np.asarray(random.rand(v, -scale, scale))
-
-        self.act_cache = {}  # cache for the activations for the backward pass
-
-    def tanh(self, x):
-        return np.tanh(x)
+        self.fc2_weights = np.asarray(rng.rand(h * v, -scale, scale), dtype=np.float32).reshape(v, h).T
+        self.fc2_bias = np.asarray(rng.rand(v, -scale, scale), dtype=np.float32)
+        # cache for the activations for the backward pass
+        self.cache = {}
 
     def parameters(self):
         return {
@@ -48,7 +46,7 @@ class MLP:
     def __call__(self, idx, targets):
         return self.forward(idx, targets)
 
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         # idx are the input tokens, (B, T) numpy array of integers
         # targets are the target tokens, (B, ) tensor of integers
         B, T = idx.shape
@@ -57,65 +55,75 @@ class MLP:
         # concat all of the embeddings together
         emb = emb.reshape(B, -1) # (B, T * embedding_size)
         # forward through the MLP
-        h = self.tanh(np.dot(emb, self.fc1_weights) + self.fc1_bias)
-        logits = np.dot(h, self.fc2_weights) + self.fc2_bias
+        hidden = np.tanh(np.dot(emb, self.fc1_weights) + self.fc1_bias)
+        logits = np.dot(hidden, self.fc2_weights) + self.fc2_bias
 
-        self.act_cache['idx'] = idx
-        self.act_cache['targets'] = targets
-        self.act_cache['emb'] = emb
-        self.act_cache['h'] = h
+        # cache some of the activations for the backward pass later
+        self.cache['idx'] = idx
+        self.cache['targets'] = targets
+        self.cache['emb'] = emb
+        self.cache['hidden'] = hidden
 
+        # if we are given desired targets, also calculate the loss
         loss = None
+        self.cache['probs'] = None
         if targets is not None:
-            # cross-entropy loss
-            exp_logits = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+            # cross-entropy loss, equivalent to F.cross_entropy in PyTorch
+            logits_max = np.max(logits, axis=1, keepdims=True)
+            exp_logits = np.exp(logits - logits_max)
             probs = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
-            correct_logprobs = -np.log(probs[np.arange(len(targets)), targets])
-            loss = np.sum(correct_logprobs) / len(targets)
-            self.act_cache['probs'] = probs
+            probs_targets = probs[np.arange(len(targets)), targets]
+            nlls = -np.log(probs_targets)
+            loss = np.mean(nlls)
+            self.cache['probs'] = probs
 
         return logits, loss
 
     def backward(self):
         # extract the activations from the forward pass
-        idx = self.act_cache['idx']
-        targets = self.act_cache['targets']
-        emb = self.act_cache['emb']
-        h = self.act_cache['h']
-        probs = self.act_cache['probs']
-        B, T = idx.shape
+        idx = self.cache['idx']
+        targets = self.cache['targets']
+        emb = self.cache['emb']
+        hidden = self.cache['hidden']
+        probs = self.cache['probs']
+        B, T = idx.shape # batch, time
 
-        # compute the gradients
-        dL_dlogits = probs
-        dL_dlogits[np.arange(len(targets)), targets] -= 1
-        dL_dlogits /= len(targets)
-
-        dL_dfc2_weights = np.dot(h.T, dL_dlogits)
-        dL_dfc2_bias = np.sum(dL_dlogits, axis=0)
-
-        dL_dh = np.dot(dL_dlogits, self.fc2_weights.T)
-        dL_dfc1 = dL_dh * (1 - h ** 2)
-
-        dL_dfc1_weights = np.dot(emb.T, dL_dfc1)
-        dL_dfc1_bias = np.sum(dL_dfc1, axis=0)
-
-        dL_emb = np.dot(dL_dfc1, self.fc1_weights.T).reshape(B, T, self.E)
-        dL_dwte = np.zeros_like(self.wte)
+        # backward through the cross entropy loss
+        dlogits = probs
+        dlogits[np.arange(len(targets)), targets] -= 1
+        dlogits /= len(targets)
+        # backward through the last linear layer of the MLP
+        dfc2_weights = np.dot(hidden.T, dlogits)
+        dfc2_bias = np.sum(dlogits, axis=0)
+        dhidden = np.dot(dlogits, self.fc2_weights.T)
+        # backward through the tanh activation
+        dprehidden = dhidden * (1 - hidden ** 2)
+        # backward through the first linear layer of the MLP
+        dfc1_weights = np.dot(emb.T, dprehidden)
+        dfc1_bias = np.sum(dprehidden, axis=0)
+        demb = np.dot(dprehidden, self.fc1_weights.T).reshape(B, T, self.embedding_size)
+        # backward through the embedding table
+        dwte = np.zeros_like(self.wte)
+        # TODO: iirc there is a vectorized way to do this
         for i in range(B):
             for j in range(T):
-                dL_dwte[idx[i, j]] += dL_emb[i, j]
+                dwte[idx[i, j]] += demb[i, j]
 
         return {
-            'wte': dL_dwte,
-            'fc1_weights': dL_dfc1_weights,
-            'fc1_bias': dL_dfc1_bias,
-            'fc2_weights': dL_dfc2_weights,
-            'fc2_bias': dL_dfc2_bias
+            'wte': dwte,
+            'fc1_weights': dfc1_weights,
+            'fc1_bias': dfc1_bias,
+            'fc2_weights': dfc2_weights,
+            'fc2_bias': dfc2_bias
         }
+
+# -----------------------------------------------------------------------------
+# AdamW optimizer
 
 class AdamW:
 
     def __init__(self, params, lr=1e-3, beta1=0.9, beta2=0.999, weight_decay=1e-4, eps=1e-8):
+        self.params = params
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
@@ -124,18 +132,18 @@ class AdamW:
         self.t = 0
         self.m = {k: np.zeros_like(v) for k, v in params.items()}
         self.v = {k: np.zeros_like(v) for k, v in params.items()}
-        self.params = params
+
+    def set_lr(self, lr):
+        self.lr = lr
 
     def step(self, grads):
         self.t += 1
         for k in self.params.keys():
             self.m[k] = self.beta1 * self.m[k] + (1 - self.beta1) * grads[k]
-            self.v[k] = self.beta2 * self.v[k] + (1 - self.beta2) * grads[k] ** 2
-            m_hat = self.m[k] / (1 - self.beta1 ** self.t)
-            v_hat = self.v[k] / (1 - self.beta2 ** self.t)
-            self.params[k] -= self.lr * m_hat / (np.sqrt(v_hat) + self.eps)
-            if self.weight_decay > 0:
-                self.params[k] -= lr * self.weight_decay * self.params[k]
+            self.v[k] = self.beta2 * self.v[k] + (1 - self.beta2) * grads[k]**2
+            m_hat = self.m[k] / (1 - self.beta1**self.t)
+            v_hat = self.v[k] / (1 - self.beta2**self.t)
+            self.params[k] -= self.lr * (m_hat / (np.sqrt(v_hat) + self.eps) + self.weight_decay * self.params[k])
 
 # -----------------------------------------------------------------------------
 # simple DataLoader that iterates over all the n-grams
@@ -172,13 +180,12 @@ def eval_split(model, tokens, max_batches=None):
     for _ in range(num_batches):
         inputs, targets = next(data_iter)
         logits, loss = model(inputs, targets)
-        total_loss += loss
+        total_loss += loss.item()
     mean_loss = total_loss / num_batches
     return mean_loss
 
 # -----------------------------------------------------------------------------
 # let's train!
-random = RNG(1337)
 
 # "train" the Tokenizer, so we're able to map between characters and tokens
 train_text = open('data/train.txt', 'r').read()
@@ -195,33 +202,38 @@ train_tokens = [char_to_token[c] for c in open('data/train.txt', 'r').read()]
 
 # create the model
 context_length = 3 # if 3 tokens predict the 4th, this is a 4-gram model
-embedding_size = 24
+embedding_size = 48
 hidden_size = 512
-model = MLP(random, vocab_size, context_length, embedding_size, hidden_size)
+init_rng = RNG(1337)
+model = MLP(init_rng, vocab_size, context_length, embedding_size, hidden_size)
 
 # optimizer
-learning_rate = 1e-3
+learning_rate = 7e-4
 optimizer = AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-4)
 
 # training loop
-batch_size = 64
+timer = StepTimer()
+batch_size = 128
 num_steps = 50000
 print(f'num_steps {num_steps}, num_epochs {num_steps * batch_size / len(train_tokens):.2f}')
 train_data_iter = dataloader(train_tokens, context_length, batch_size)
 for step in range(num_steps):
     # cosine learning rate schedule, from max lr to 0
     lr = learning_rate * 0.5 * (1 + math.cos(math.pi * step / num_steps))
+    optimizer.set_lr(lr)
     # every now and then evaluate the validation loss
     last_step = step == num_steps - 1
     if step % 200 == 0 or last_step:
         train_loss = eval_split(model, train_tokens, max_batches=20)
         val_loss = eval_split(model, val_tokens)
-        print(f'step {step}/{num_steps} | train_loss {train_loss:.4f} | val_loss {val_loss:.4f} | lr {lr:e}')
-    # get the next batch of training data
-    inputs, targets = next(train_data_iter)
-    # forward through the model
-    logits, loss = model(inputs, targets)
-    # backpropagate and update the weights
-    grads = model.backward()
-    # step the optimizer
-    optimizer.step(grads)
+        print(f'step {step:6d} | train_loss {train_loss:.6f} | val_loss {val_loss:.6f} | lr {lr:e} | time/step {timer.get_dt()*1000:.4f}ms')
+    # training step
+    with timer:
+        # get the next batch of training data
+        inputs, targets = next(train_data_iter)
+        # forward through the model
+        logits, loss = model(inputs, targets)
+        # backpropagate and update the weights
+        grads = model.backward()
+        # step the optimizer
+        optimizer.step(grads)
